670e089d458a31858c48548ba5a10f6d18788364
Bogdan Finn <bogdan.finn@googlemail.com>
2022-07-11 22:24:31 +0200

    updated example to compare profiles against topps;

--- example/main.go
+++ example/main.go
@@ -2,14 +2,53 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
+	goHttp "net/http"
 
 	http "github.com/Carcraftz/fhttp"
 	tls_client "tls-client"
 )
 
 func main() {
+	requestToppsAsGoClient()
+	requestToppsAsChrome103Client()
+}
+
+func requestToppsAsGoClient() {
+	c := &goHttp.Client{}
+
+	r, err := goHttp.NewRequest(http.MethodGet, "https://www.topps.com/", nil)
+
+	r.Header = goHttp.Header{
+		"accept":                    {"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"},
+		"accept-encoding":           {"gzip"},
+		"Accept-Encoding":           {"gzip"},
+		"accept-language":           {"de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7"},
+		"cache-control":             {"max-age=0"},
+		"if-none-match":             {`W/"4d0b1-K9LHIpKrZsvKsqNBKd13iwXkWxQ"`},
+		"sec-ch-ua":                 {`" Not A;Brand";v="99", "Chromium";v="101", "Google Chrome";v="101"`},
+		"sec-ch-ua-mobile":          {"?0"},
+		"sec-ch-ua-platform":        {`"macOS"`},
+		"sec-fetch-dest":            {"document"},
+		"sec-fetch-mode":            {"navigate"},
+		"sec-fetch-site":            {"none"},
+		"sec-fetch-user":            {"?1"},
+		"upgrade-insecure-requests": {"1"},
+		"user-agent":                {"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36"},
+	}
+
+	re, err := c.Do(r)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	defer re.Body.Close()
+
+	log.Println(fmt.Sprintf("requesting topps as golang => status code: %d", re.StatusCode))
+}
+
+func requestToppsAsChrome103Client() {
 	options := []tls_client.HttpClientOption{
 		tls_client.WithTimeout(30),
 		tls_client.WithClientProfile(tls_client.Chrome_103),
@@ -21,7 +60,7 @@ func main() {
 		return
 	}
 
-	req, err := http.NewRequest(http.MethodGet, "https://tls.peet.ws/api/all", nil)
+	req, err := http.NewRequest(http.MethodGet, "https://www.topps.com/", nil)
 	if err != nil {
 		log.Println(err)
 		return
@@ -34,7 +73,6 @@ func main() {
 		"accept-language":           {"de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7"},
 		"cache-control":             {"max-age=0"},
 		"if-none-match":             {`W/"4d0b1-K9LHIpKrZsvKsqNBKd13iwXkWxQ"`},
-		"referer":                   {"https://www.asos.com/de/damen/ctas/styles-fur-besondere-anlasse/cat/?cid=13502&ctaref=hp%7Cww%7Cprime%7Cfeature%7C2%7Ccategory%7Csummerdresses&page=1&scrollTo=product-200985291"},
 		"sec-ch-ua":                 {`" Not A;Brand";v="99", "Chromium";v="101", "Google Chrome";v="101"`},
 		"sec-ch-ua-mobile":          {"?0"},
 		"sec-ch-ua-platform":        {`"macOS"`},
@@ -71,13 +109,5 @@ func main() {
 
 	defer resp.Body.Close()
 
-	readBytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	log.Println(string(readBytes))
-
-	log.Println(fmt.Sprintf("status code: %d", resp.StatusCode))
+	log.Println(fmt.Sprintf("requesting topps as chrome103 => status code: %d", resp.StatusCode))
 }
