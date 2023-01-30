55259dc5994def4a97c5f44d32e6812f1b260204
Bogdan Finn <bogdan.finn@googlemail.com>
2023-01-30 21:17:50 +0100

    wrapping net/http interface for missing methods;

--- client.go
+++ client.go
@@ -309,6 +309,33 @@ func (c *httpClient) Do(req *http.Request) (*http.Response, error) {
 	return resp, nil
 }
 
+func (c *httpClient) Get(url string) (resp *http.Response, err error) {
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return c.Do(req)
+}
+func (c *httpClient) Head(url string) (resp *http.Response, err error) {
+	req, err := http.NewRequest(http.MethodHead, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return c.Do(req)
+}
+func (c *httpClient) Post(url, contentType string, body io.Reader) (resp *http.Response, err error) {
+	req, err := http.NewRequest(http.MethodPost, url, body)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", contentType)
+
+	return c.Do(req)
+}
+
 func allToLower(list []string) []string {
 	var lower []string
 
