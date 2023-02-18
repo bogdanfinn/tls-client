46e88e37c8794c92836b1b09ae5dda7acb32ed68
Bogdan Finn <bogdan.finn@googlemail.com>
2023-02-18 22:26:11 +0100

    updated shared libraries;

--- cffi_dist/example_node/index_memory.js
+++ cffi_dist/example_node/index_memory.js
@@ -6,7 +6,7 @@ class TLS {
     }
 
     initTLS() {
-        let tlsLib = "./../dist/tls-client-darwin-amd64-1.3.3.dylib";
+        let tlsLib = "./../dist/tls-client-darwin-amd64-1.3.4.dylib";
         return ffi.Library(tlsLib, {
             request: ["string", ["string"]],
             freeMemory: ["void", ['string']],
