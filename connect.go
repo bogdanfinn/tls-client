f6f1ab14489ed59a66dc9a715485d3e17cc82cd7
Adam Roach <adamroach504@gmail.com>
2023-04-08 23:09:52 -0500

    fix io timeout

--- connect.go
+++ connect.go
@@ -217,19 +217,6 @@ func (c *connectDialer) DialContext(ctx context.Context, network, address string
 			return nil, err
 		}
 
-		deadline := time.Now().Add(c.Timeout)
-
-		err = rawConn.SetReadDeadline(deadline)
-		if err != nil {
-			_ = rawConn.Close()
-			return nil, err
-		}
-		err = rawConn.SetWriteDeadline(deadline)
-		if err != nil {
-			_ = rawConn.Close()
-			return nil, err
-		}
-
 		resp, err := http.ReadResponse(bufio.NewReader(rawConn), req)
 		if err != nil {
 			_ = rawConn.Close()
