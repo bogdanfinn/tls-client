b9bde8e07c8b4c2226fe03ac2cac393a96104a2b
justhyped <justhyped@protonmail.com>
2023-05-15 11:37:34 +0200

    various improvements to `Logger`
    - add interface guards to make sure all methods are implemented
    - _ as argument names for unused args

--- logger.go
+++ logger.go
@@ -3,10 +3,10 @@ package tls_client
 import "fmt"
 
 type Logger interface {
-	Debug(format string, args ...interface{})
-	Info(format string, args ...interface{})
-	Warn(format string, args ...interface{})
-	Error(format string, args ...interface{})
+	Debug(format string, args ...any)
+	Info(format string, args ...any)
+	Warn(format string, args ...any)
+	Error(format string, args ...any)
 }
 
 type noopLogger struct {
@@ -16,13 +16,13 @@ func NewNoopLogger() Logger {
 	return &noopLogger{}
 }
 
-func (n noopLogger) Debug(format string, args ...interface{}) {}
+func (n noopLogger) Debug(_ string, _ ...any) {}
 
-func (n noopLogger) Info(format string, args ...interface{}) {}
+func (n noopLogger) Info(_ string, _ ...any) {}
 
-func (n noopLogger) Warn(format string, args ...interface{}) {}
+func (n noopLogger) Warn(_ string, _ ...any) {}
 
-func (n noopLogger) Error(format string, args ...interface{}) {}
+func (n noopLogger) Error(_ string, _ ...any) {}
 
 type debugLogger struct {
 	logger Logger
@@ -34,19 +34,19 @@ func NewDebugLogger(logger Logger) Logger {
 	}
 }
 
-func (n debugLogger) Debug(format string, args ...interface{}) {
+func (n debugLogger) Debug(format string, args ...any) {
 	fmt.Printf(format+"\n", args...)
 }
 
-func (n debugLogger) Info(format string, args ...interface{}) {
+func (n debugLogger) Info(format string, args ...any) {
 	n.logger.Info(format, args...)
 }
 
-func (n debugLogger) Warn(format string, args ...interface{}) {
+func (n debugLogger) Warn(format string, args ...any) {
 	n.logger.Warn(format, args...)
 }
 
-func (n debugLogger) Error(format string, args ...interface{}) {
+func (n debugLogger) Error(format string, args ...any) {
 	n.logger.Error(format, args...)
 }
 
@@ -56,16 +56,21 @@ func NewLogger() Logger {
 	return &logger{}
 }
 
-func (n logger) Debug(format string, args ...interface{}) {}
+func (n logger) Debug(_ string, _ ...any) {}
 
-func (n logger) Info(format string, args ...interface{}) {
+func (n logger) Info(format string, args ...any) {
 	fmt.Printf(format+"\n", args...)
 }
 
-func (n logger) Warn(format string, args ...interface{}) {
+func (n logger) Warn(format string, args ...any) {
 	fmt.Printf(format+"\n", args...)
 }
 
-func (n logger) Error(format string, args ...interface{}) {
+func (n logger) Error(format string, args ...any) {
 	fmt.Printf(format+"\n", args...)
 }
+
+// Interface guards are a cheap way to make sure all methods are implemented, this is a static check and does not affect runtime performance.
+var _ Logger = (*logger)(nil)
+var _ Logger = (*debugLogger)(nil)
+var _ Logger = (*noopLogger)(nil)
