e35d465d1eae50ae15d51729ac14b322d8b694b2
Bogdan Finn <bogdan.finn@googlemail.com>
2023-07-30 00:33:07 +0200

    fixed shared library version in examples;

--- cffi_dist/example_python/example_custom_client.py
+++ cffi_dist/example_python/example_custom_client.py
@@ -2,7 +2,7 @@ import ctypes
 import json
 
 # load the tls-client shared package for your OS you are currently running your python script (i'm running on mac)
-library = ctypes.cdll.LoadLibrary('./../dist/tls-client-darwin-amd64-1.4.1.dylib')
+library = ctypes.cdll.LoadLibrary('./../dist/tls-client-darwin-amd64-1.5.0.dylib')
 
 # extract the exposed request function from the shared package
 request = library.request
