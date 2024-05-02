5c23048cd0d3dbe4fcc82fdb1ac3a53ee3f2173d
josh-pinwheelapi <josh@pinwheelapi.com>
2024-05-02 16:38:35 -0400

    Add comment

--- cffi_dist/build.sh
+++ cffi_dist/build.sh
@@ -3,8 +3,8 @@
 # This build.sh file was created on a OSX host system. If you are running on windows / unix you need to adjust the commands accordingly.
 
 echo 'Build OSX'
-GOOS=darwin CGO_ENABLED=1 GOARCH=arm64 go build -buildmode=c-shared -o ./dist/tls-client-darwin-arm64-sonoma.dylib
-GOOS=darwin CGO_ENABLED=1 GOARCH=amd64 go build -buildmode=c-shared -o ./dist/tls-client-darwin-amd64-sonoma.dylib
+#GOOS=darwin CGO_ENABLED=1 GOARCH=arm64 go build -buildmode=c-shared -o ./dist/tls-client-darwin-arm64-sonoma.dylib
+#GOOS=darwin CGO_ENABLED=1 GOARCH=amd64 go build -buildmode=c-shared -o ./dist/tls-client-darwin-amd64-sonoma.dylib
 
 # echo 'Build Linux ARM64'
 # # CC is needed when you cross compile from OSX to Linux
@@ -30,7 +30,8 @@ echo 'Build Linux Ubuntu'
 # For some reason my OSX gcc cross compiler does not work. Therefore i use a ubuntu docker image
 # GOOS=linux CGO_ENABLED=1 GOARCH=amd64 CC="x86_64-linux-musl-gcc" go build -buildmode=c-shared -o ./dist/tls-client-linux-amd64.so
 # Make sure to first build the image based on the Dockerfile.ubuntu.compile in this directory.
-docker run -v $PWD/../:/tls-client tls-client-ubuntu-go-1.20 bash -c "cd /tls-client/cffi_dist && GOOS=linux CGO_ENABLED=1 GOARCH=amd64 go build -buildmode=c-shared -o /tls-client/cffi_dist/dist/tls-client-linux-ubuntu-amd64-$1.so"
+# docker build  --platform linux/amd64 . -t tls-client-ubuntu-go-1.20
+docker run -v $PWD/../:/tls-client tls-client-ubuntu-go-1.20 bash -c "cd /tls-client/cffi_dist && GOOS=linux CGO_ENABLED=1 GOARCH=amd64 go build -buildmode=c-shared -o /tls-client/cffi_dist/dist/tls-client-linux-ubuntu-amd64-.so"
 
 
 # # CC is needed when you cross compile from OSX to Windows
