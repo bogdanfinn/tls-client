84d3328087ab1c2f8c1ffb77b21782a3f0893918
Morozov Roman <r-r-roma-a-a@yandex.ru>
2024-05-13 11:29:20 +0400

    do not lose context

--- roundtripper.go
+++ roundtripper.go
@@ -100,7 +100,7 @@ func (rt *roundTripper) getTransport(req *http.Request, addr string) error {
 		return fmt.Errorf("invalid URL scheme: [%v]", req.URL.Scheme)
 	}
 
-	_, err := rt.dialTLS(context.Background(), "tcp", addr)
+	_, err := rt.dialTLS(req.Context(), "tcp", addr)
 	switch err {
 	case errProtocolNegotiated:
 	case nil:
