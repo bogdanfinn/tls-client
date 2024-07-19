f0124c3b8775c8acc42735ed7620fda17753197a
tigersoft6084 <deanpetre672@proton.me>
2024-07-19 09:34:44 +0200

    fixed StreamOuputPath

--- cffi_src/factory.go
+++ cffi_src/factory.go
@@ -15,7 +15,7 @@ import (
 	http "github.com/bogdanfinn/fhttp"
 	"github.com/bogdanfinn/fhttp/cookiejar"
 	"github.com/bogdanfinn/fhttp/http2"
-	"github.com/bogdanfinn/tls-client"
+	tls_client "github.com/bogdanfinn/tls-client"
 	tls "github.com/bogdanfinn/utls"
 	"github.com/google/uuid"
 )
@@ -143,12 +143,16 @@ func BuildRequest(input RequestInput) (*http.Request, *TLSClientError) {
 func readAllBodyWithStreamToFile(respBody io.ReadCloser, input RequestInput) ([]byte, error) {
 	var respBodyBytes []byte
 	var err error
-
+	var bodyLen = 0
 	f, err := os.OpenFile(*input.StreamOutputPath, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0o600)
 	if err != nil {
 		return nil, err
 	}
-	defer f.Close()
+	defer func() {
+		if closeErr := f.Close(); closeErr != nil {
+			fmt.Printf("failed to close file: %v\n", closeErr)
+		}
+	}()
 
 	blockSize := 1024 // 1 KB
 	if input.StreamOutputBlockSize != nil {
@@ -157,26 +161,29 @@ func readAllBodyWithStreamToFile(respBody io.ReadCloser, input RequestInput) ([]
 	buf := make([]byte, blockSize)
 	// Read the response body
 	for {
-		n, err := respBody.Read(buf)
-		if err == io.EOF {
+		n, readErr := respBody.Read(buf)
+		bodyLen += n
+		fmt.Printf("Reading at: %d\n", bodyLen)
+		if _, err = f.Write(buf[:n]); err != nil {
+			// if input.WithDebug {
+			fmt.Printf("Append stream output error: %+v\n", err)
+			// }
+
+			return nil, err
+		}
+
+		if readErr == io.EOF {
+
 			if input.StreamOutputEOFSymbol != nil {
 				f.Write([]byte(*input.StreamOutputEOFSymbol))
 			}
 
 			break
-		}
-
-		respBodyBytes = append(respBodyBytes, buf[:n]...)
-		if _, err = f.Write(buf[:n]); err != nil {
-			if input.WithDebug {
-				fmt.Printf("Append stream output error: %+v\n", err)
-			}
-
-			return nil, err
-		}
-
-		if input.WithDebug {
-			fmt.Printf("[stream decode result]==========\n%+v\n==========\n", string(buf[:n]))
+		} else if readErr != nil {
+			// if input.WithDebug {
+			fmt.Printf("Body error: %+v\n", readErr)
+			// }
+			return nil, readErr
 		}
 	}
 
