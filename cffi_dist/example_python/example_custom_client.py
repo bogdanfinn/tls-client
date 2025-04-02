f015f606063e359878f0ff5e19c5c53b3e81fd76
Bogdan Finn <kontakt@bogdanfinn.de>
2025-04-02 08:46:16 +0200

    updated examples and mapper;

--- cffi_dist/example_python/example_custom_client.py
+++ cffi_dist/example_python/example_custom_client.py
@@ -67,7 +67,7 @@ requestPayload = {
         ],
         "supportedVersions": ["GREASE", "1.3", "1.2"],
         "keyShareCurves": ["GREASE", "X25519"],
-        "certCompressionAlgo": "brotli",
+        "certCompressionAlgos": ["brotli"],
         "alpnProtocols": ["h2", "http/1.1"],
         "alpsProtocols": ["h2"],
         "pseudoHeaderOrder": [
